e3f00939eaceee80edffc37f4b19aa784ccaea28
rcarra-arq <rcarra.arq@gmail.com>
2026-06-07 23:19:04 +0000

    Initial Terraform and GitHub Actions setup

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+
+  key_name = "vockey"
+
+  tags = {
+    Name = "terraform-demo"
+  }
+}
